82fd2ac5e5e969a59e6df07fe0b6f1cd278221ae
Cristian Colosimo <cristian.colosimo@exabit.it>
2023-05-23 22:44:11 +0200

    cambiato il controllo del presence in exec

--- src/services/hpccInternal.ts
+++ src/services/hpccInternal.ts
@@ -1,4 +1,5 @@
-import rpio from 'rpio';
+import { exec } from 'child_process';
+import fs from 'fs';
 /**
 LOCK_PIN = getattr(settings, 'LOCK_PIN', 4)
 MAGNET_PIN = getattr(settings, 'MAGNET_PIN', 17)
@@ -11,17 +12,22 @@ const LOCK_PIN = 4;
 const MAGNET_PIN = 17;
 
 const PULSE_SLEEP = 1000; //1s 1000ms
-const PULSE_ON = rpio.HIGH;
-const PULSE_OFF = rpio.LOW;
+const PULSE_ON = 0;
+const PULSE_OFF = 1;
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 class HpccInternal {
   constructor() {}
   async setup() {
     try {
-      rpio.open(LOCK_PIN, rpio.OUTPUT);
-      rpio.open(MAGNET_PIN, rpio.INPUT);
-      
+      exec(`echo ${LOCK_PIN} > /sys/class/gpio/export`);
+      exec(`echo ${MAGNET_PIN} > /sys/class/gpio/export`);
+      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+      exec(`echo out > /sys/class/gpio/gpio${LOCK_PIN}/direction`);
+      exec(`echo in > /sys/class/gpio/gpio${MAGNET_PIN}/direction`);
+      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+
       return true;
     } catch (e) {
       console.log(e);
@@ -31,9 +37,9 @@ class HpccInternal {
 
   async send_unlock_pulse() {
     try {
-      rpio.write(LOCK_PIN, PULSE_ON);
+      exec(`echo ${PULSE_ON} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
       await sleep(PULSE_SLEEP);
-      rpio.write(LOCK_PIN, PULSE_OFF);
+      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
       return true;
     } catch (e) {
       console.error(e);
@@ -42,8 +48,8 @@ class HpccInternal {
   }
   async is_magnet_on() {
     try {
-      const value = rpio.read(MAGNET_PIN);
-      return value;
+      const value = parseInt(fs.readFileSync(`/sys/class/gpio/gpio${MAGNET_PIN}/value`, {encoding:'utf8'})) 
+      return !value;
     } catch (e) {
       console.error(e);
       return false;
