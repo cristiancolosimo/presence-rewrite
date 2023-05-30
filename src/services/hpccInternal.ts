230b06ef659ede4060645d67c704ee9f7aa366b4
Cristian Colosimo <cristian.colosimo@exabit.it>
2023-05-30 23:24:37 +0200

    Changed the gpio library for testing to pigpio

--- src/services/hpccInternal.ts
+++ src/services/hpccInternal.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 import fs from 'fs';
+import {Gpio} from 'pigpio';
 /**
 LOCK_PIN = getattr(settings, 'LOCK_PIN', 4)
 MAGNET_PIN = getattr(settings, 'MAGNET_PIN', 17)
@@ -15,18 +16,18 @@ const PULSE_SLEEP = 1000; //1s 1000ms
 const PULSE_ON = 0;
 const PULSE_OFF = 1;
 
+
+
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 class HpccInternal {
+  LOCK_PIN_GPIO_INT : Gpio | null = null;
   constructor() {}
   async setup() {
     try {
-      exec(`echo ${LOCK_PIN} > /sys/class/gpio/export`);
       exec(`echo ${MAGNET_PIN} > /sys/class/gpio/export`);
-      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
       exec(`echo out > /sys/class/gpio/gpio${LOCK_PIN}/direction`);
       exec(`echo in > /sys/class/gpio/gpio${MAGNET_PIN}/direction`);
-      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
-      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+      this.LOCK_PIN_GPIO_INT = new Gpio(LOCK_PIN, {mode: Gpio.OUTPUT, pullUpDown: Gpio.PUD_OFF });
 
       return true;
     } catch (e) {
@@ -37,9 +38,9 @@ class HpccInternal {
 
   async send_unlock_pulse() {
     try {
-      exec(`echo ${PULSE_ON} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+      this.LOCK_PIN_GPIO_INT?.digitalWrite(PULSE_ON);
       await sleep(PULSE_SLEEP);
-      exec(`echo ${PULSE_OFF} > /sys/class/gpio/gpio${LOCK_PIN}/value`);
+      this.LOCK_PIN_GPIO_INT?.digitalWrite(PULSE_OFF);
       return true;
     } catch (e) {
       console.error(e);
